059e134b3b4125cd5b8c2d2d58859913427e09f5
Ale-Miret <112821785+Ale-Miret@users.noreply.github.com>
2023-03-25 01:28:15 -0400

    commented code

--- src/htmlCreator.js
+++ src/htmlCreator.js
@@ -1,19 +1,27 @@
+// Import necessary functions
 const { createEngineer, createIntern, createManager, createEmployeesPage } = require('./html-temp');
 
+
+// Define function to insert HTML
 const insertHTML = (employees) => {
+    // Create object to map role to corresponding card creator function
     const roleCreator = {
       Manager: createManager,
       Engineer: createEngineer,
       Intern: createIntern,
     };
 
-    const cards = employees.map(({ getRole, ...employee }) => {
+// Map employees to their corresponding card creator function and join the results into a single string of HTML cards
+const cards = employees.map(({ getRole, ...employee }) => {
         const cardCreator = roleCreator[getRole()];
         return cardCreator(employee);
       }).join('');
     
-    const createEmployees = createEmployeesPage(createCards);
+
+// Create HTML page using the createEmployeesPage function and the HTML cards
+const createEmployees = createEmployeesPage(createCards);
     return createEmployees;
 };
 
+// Export the insertHTML function
 module.exports = insertHTML;
